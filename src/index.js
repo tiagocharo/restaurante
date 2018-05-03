d68d56ed4768bb03a5d5daf919b42a0153fde0a0
Tiago Rocha <tiagofarocha@yahoo.com.br>
2018-05-03 16:34:22 -0300

    upgrade

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './routes/Routes';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
-registerServiceWorker();
